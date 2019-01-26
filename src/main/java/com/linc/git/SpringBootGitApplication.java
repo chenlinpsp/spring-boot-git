5e4aaf7123669689a4f5d6f891a56a8f1dff98f1
chenlinpsp <chenlinpsp@163.com>
2019-01-26 11:44:41 +0800

    init project: spring-boot-git

---
+++ src/main/java/com/linc/git/SpringBootGitApplication.java
@@ -0,0 +1,14 @@
+package com.linc.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGitApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/linc/git/SpringBootGitApplicationTests.java
@@ -0,0 +1,17 @@
+package com.linc.git;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootGitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
